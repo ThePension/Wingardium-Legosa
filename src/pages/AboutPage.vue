05866a74c20d2d500d2a6b54567a8eb6cf8c7fa8
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-03-27 21:36:29 +0200

    Add simple camera with snapshot feature (#1)

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>This is an about page</p>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AboutPage",
+});
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,57 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <div>
+    <div v-if="!cameraActive">
+      <button @click="startCamera">Démarrer la caméra</button>
+    </div>
+    <div v-else>
+      <button @click="takePicture">Prendre une photo</button>
+      <camera
+        :resolution="{ width: 375, height: 812 }"
+        ref="camera"
+        autoplay
+      ></camera>
+    </div>
+    <div v-if="pictureTaken">
+      <img :src="pictureData" />
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import Camera from "simple-vue-camera";
+import { ref } from "vue";
 
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  name: "IndexPage",
+  components: {
+    Camera,
+  },
+
+  setup() {
+    const cameraActive = ref(false);
+    const pictureTaken = ref(false);
+    const pictureData = ref(null);
+    const camera = ref(Camera);
+
+    const startCamera = () => {
+      cameraActive.value = true;
+    };
+
+    const takePicture = async () => {
+      console.log(camera.value.snapshot);
+      const picture = await camera.value.snapshot();
+      pictureData.value = URL.createObjectURL(picture);
+      pictureTaken.value = true;
+    };
+
+    return {
+      cameraActive,
+      pictureTaken,
+      pictureData,
+      camera,
+      startCamera,
+      takePicture,
+    };
+  },
+};
 </script>
