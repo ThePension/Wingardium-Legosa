1e6378d17bc0c5c9388a17bc8fc7b0b46e47e027
Nicolas Aubert <nicolas.aubert1@he-arc.ch>
2023-04-03 09:26:00 +0200

    Load image from file

--- src/pages/ResultPage.vue
+++ src/pages/ResultPage.vue
@@ -3,17 +3,33 @@ import { ref, onMounted } from "vue";
 import { grayscale } from "../services/LegoAnalyzer.js";
 import cv from "@techstark/opencv-js";
 
-// Load image from file as a cv Mat
-const mat = cv.imread("../data/legos.jpg");
+const imgUrl = ref(null);
+const img = ref(null);
 
 const pictureData = ref(null);
 
-onMounted(() => {
+const processImage = () => {
+  const mat = cv.imread(img);
   pictureData.value = grayscale(mat);
-});
+};
+
+const updateMatFromUrl = (event) => {
+  img.value = event.target.files[0];
+
+  imgUrl.value = URL.createObjectURL(img.value);
+
+  processImage();
+};
+
+// onMounted(() => {
+//   pictureData.value = grayscale(mat);
+// });
 </script>
 
 <template>
   <h1>ResultPage</h1>
+  <img :src="imgUrl" style="width: 30%; height: 30%" />
   <img :src="pictureData" />
+  <!-- Image Input, that call updateMatFromUrl onchange -->
+  <input type="file" accept="image/*" @change="updateMatFromUrl" />
 </template>
